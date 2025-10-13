a132929600c4a1bf2e2c798b8c1b048d7d10e1fb
vishnusomisetty1 <Vishnusomisetty14@gmail.com>
2025-10-13 08:17:39 -0400

    Initial Commit

---
+++ Flowly-calendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flowly-calendar
+//
+//  Created by Vishnu Somisetty on 10/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
